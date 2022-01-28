347e5c4ce522d7883329b67cb0b238b4896a02dd
zoltan.szitas <zoltan.szitas@artinsolutions.com>
2022-01-28 16:31:24 +0100

    AJ-2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
 <div class="app">
   <Navbar />
+  <Footer />
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 });
 </script>
