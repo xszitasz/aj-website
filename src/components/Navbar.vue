845e70fed5b6f77f88f335fc9b5313a1ce3fa25c
zoltan.szitas <zoltan.szitas@artinsolutions.com>
2022-01-28 16:53:03 +0100

    AJ-4

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,8 +11,8 @@
         <a href="https://www.facebook.com/profile.php?id=100017322969115" class="facebook"><i class="bi bi-facebook"></i></a>
         <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
         <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-        <a @click="changeLanguage('SVK')">{{ language }}</a>
-        <a @click="changeLanguage('HUN')">{{ language }}</a>
+        <a @click="changeLanguage('SVK')">SVK</a>
+        <a @click="changeLanguage('HUN')">HUN</a>
       </div>
 
     </div>
@@ -24,61 +24,65 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
     data() {
-        return {
-            email: 'xszitasz@gmail.com',
-            language: 'SVK',
-            phoneNumber: '+421 910 623 309'
-        }
+      return {
+        email: 'xszitasz@gmail.com',
+        language: 'SVK',
+        phoneNumber: '+421 910 623 309'
+      }
     },
     methods: {
-        changeLanguage(language: string){
-            if(language === 'HUN') {
-                this.phoneNumber = '+421 910 623 309';
-            } else {
-                this.phoneNumber = '+36 910 623 309';
-            }
-        }
+      changeLanguage(language: string){
+        if(language === 'HUN') { this.phoneNumber = '+421 910 623 309'; } 
+        else { this.phoneNumber = '+36 910 623 309'; }
+      }
     }
 })
 </script>
 
 <style scoped>
-#topbar {
-  background: #fbfbfb;
-  font-size: 15px;
-  height: 40px;
-  padding: 0;
-}
 
-#topbar .contact-info a {
-  line-height: 0;
-  color: #444444;
-  transition: 0.3s;
-}
+  #topbar {
+    background: #fbfbfb;
+    font-size: 15px;
+    height: 40px;
+    padding: 0;
+  }
 
-#topbar .contact-info a:hover {
-  color: #5cb874;
-}
+  #topbar .contact-info a {
+    line-height: 0;
+    color: #444444;
+    transition: 0.3s;
+  }
 
-#topbar .contact-info i {
-  color: #5cb874;
-  line-height: 0;
-  margin-right: 5px;
-}
+  #topbar .contact-info a:hover {
+    color: #5cb874;
+  }
 
-#topbar .contact-info .phone-icon {
-  margin-left: 15px;
-}
+  #topbar .contact-info i {
+    color: #5cb874;
+    line-height: 0;
+    margin-right: 5px;
+  }
 
-#topbar .social-links a {
-  color: #6f6f6f;
-  padding: 4px 12px;
-  display: inline-block;
-  line-height: 1px;
-  transition: 0.3s;
-}
+  #topbar .contact-info .phone-icon {
+    margin-left: 15px;
+  }
 
-#topbar .social-links a:hover {
-  color: #5cb874;
-}
+  #topbar .social-links a {
+    color: #6f6f6f;
+    padding: 4px 12px;
+    display: inline-block;
+    line-height: 1px;
+    transition: 0.3s;
+  }
+
+  #topbar .social-links a:hover {
+    color: #5cb874;
+  }
+
+  @media only screen and (max-width: 335px) {   
+    #topbar {
+      font-size: 14px;
+    }
+  }    
 </style>
